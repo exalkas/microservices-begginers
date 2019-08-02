ef25284c27566e7f2841d17b114d2bcc79aaed0a
alkas <rdxtbl@gmail.com>
2019-08-02 14:32:35 +0300

    chapter 2

--- database.js
+++ database.js
@@ -1,7 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/db', (req, res) => {
+    const data = "Hello this is server microservices for beginners";
+    res.send(data);
+})
+
+app.listen(4546, () => console.log("Server is up and listening at 4546"));
+
 // const databaseFunction
-module.exports = {
-    databaseFunction: () => [{
-        title : "First post",
-        content: "First content"
-    }]
-}
+// module.exports = {
+//     databaseFunction: () => [{
+//         title : "First post",
+//         content: "First content"
+//     }]
+// }
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
 const app = express();
+
+const axios = require('axios');
+
 const {databaseFunction} = require('./database');
 
-app.get('/', (req, res) => {
-    res.send(databaseFunction());
+app.get('/', async (req, res) => {
+    try {
+        const result = await axios.get('http://localhost:4546/db');
+        res.send(result.data);    
+    } catch (error) {
+        
+    }
+    
 });
 
 app.listen(3434, () => {
